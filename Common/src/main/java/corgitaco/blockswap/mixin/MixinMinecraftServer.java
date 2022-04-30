04dfdd83887b2c993799a36a52937ff19d78b234
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2022-04-30 15:33:37 -0700

    Update to 1.18.2, implements #23

--- Common/src/main/java/corgitaco/blockswap/mixin/MixinMinecraftServer.java
+++ Common/src/main/java/corgitaco/blockswap/mixin/MixinMinecraftServer.java
@@ -4,14 +4,12 @@ import com.mojang.authlib.GameProfileRepository;
 import com.mojang.authlib.minecraft.MinecraftSessionService;
 import com.mojang.datafixers.DataFixer;
 import corgitaco.blockswap.BlockSwap;
-import net.minecraft.core.RegistryAccess;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.ServerResources;
+import net.minecraft.server.WorldStem;
 import net.minecraft.server.level.progress.ChunkProgressListenerFactory;
 import net.minecraft.server.packs.repository.PackRepository;
 import net.minecraft.server.players.GameProfileCache;
 import net.minecraft.world.level.storage.LevelStorageSource;
-import net.minecraft.world.level.storage.WorldData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -23,7 +21,7 @@ import java.net.Proxy;
 public abstract class MixinMinecraftServer {
 
     @Inject(at = @At("RETURN"), method = "<init>")
-    private void loadConfig(Thread $$0, RegistryAccess.RegistryHolder $$1, LevelStorageSource.LevelStorageAccess $$2, WorldData $$3, PackRepository $$4, Proxy $$5, DataFixer $$6, ServerResources $$7, MinecraftSessionService $$8, GameProfileRepository $$9, GameProfileCache $$10, ChunkProgressListenerFactory $$11, CallbackInfo ci) {
+    private void blockSwap_loadConfig(Thread $$0, LevelStorageSource.LevelStorageAccess $$1, PackRepository $$2, WorldStem $$3, Proxy $$4, DataFixer $$5, MinecraftSessionService $$6, GameProfileRepository $$7, GameProfileCache $$8, ChunkProgressListenerFactory $$9, CallbackInfo ci) {
         BlockSwap.getConfig(true);
     }
 }
