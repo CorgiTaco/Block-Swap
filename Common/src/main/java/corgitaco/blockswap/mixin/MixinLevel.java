71edb925428fd31f90d837a79cfe0bfc1758f976
Joseph T. McQuigg <j.t.mcquigg12@gmail.com>
2023-06-20 09:29:16 -0400

    Fix MixinLevel.java
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Common/src/main/java/corgitaco/blockswap/mixin/MixinLevel.java
+++ Common/src/main/java/corgitaco/blockswap/mixin/MixinLevel.java
@@ -11,13 +11,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Level.class)
+@Mixin(value = Level.class)
 public abstract class MixinLevel {
 
     @Shadow
     public abstract boolean setBlock(BlockPos pos, BlockState state, int i, int flags);
 
-    @Inject(method = "setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;II)Z", at = @At("INVOKE"), cancellable = true)
+    @Inject(method = "setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;II)Z", at = @At("HEAD"), cancellable = true)
     private void isIncompatibleBlock(BlockPos pos, BlockState state, int i, int flags, CallbackInfoReturnable<Boolean> cir) {
         if (BlockSwap.getConfig(false).contains(state)) {
             cir.setReturnValue(setBlock(pos, Swapper.remapState(state), i, flags));
